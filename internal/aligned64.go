aaf1eced75c49f99f94263f7c24d03b1d666de64
Equoo <biobraix@outlook.fr>
2023-11-20 21:47:45 +0100

    y

--- internal/aligned64.go
+++ internal/aligned64.go
@@ -11,18 +11,26 @@ typedef struct int64aligned { int64_t value[1]; } int64aligned;
 */
 import "C"
 
-func (u64 C.uint64aligned) Get() uint64 {
-	return uint64(u64.value[0])
+type Uint64Aligned struct {
+    value C.uint64aligned
 }
 
-func (u64 *C.uint64aligned) Set(value uint64) {
-	u64.value[0] = C.uint64_t(value)
+type Int64Aligned struct {
+    value C.int64aligned
 }
 
-func (i64 C.int64aligned) Get() int64 {
-	return int64(i64.value[0])
+func (u64 *Uint64Aligned) Get() uint64 {
+    return uint64(u64.value.value[0])
 }
 
-func (i64 *C.int64aligned) Set(value int64) {
-	i64.value[0] = C.int64_t(value)
+func (u64 *Uint64Aligned) Set(value uint64) {
+    u64.value.value[0] = C.uint64_t(value)
+}
+
+func (i64 *Int64Aligned) Get() int64 {
+    return int64(i64.value.value[0])
+}
+
+func (i64 *Int64Aligned) Set(value int64) {
+    i64.value.value[0] = C.int64_t(value)
 }
