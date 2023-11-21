a7e475fa09c1cf3079311c0ef678344decf76627
Equoo <biobraix@outlook.fr>
2023-11-22 00:43:58 +0100

    fix aligned type

--- internal/aligned32.go
+++ internal/aligned32.go
@@ -11,20 +11,20 @@ typedef struct int64aligned { uint32_t value[2]; } int64aligned;
 */
 import "C"
 
-func (u64 C.uint64aligned) Get() uint64 {
+func GetUint64(u64 C.uint64aligned) uint64 {
 	return uint64(u64.value[0]) | uint64(u64.value[1])<<32
 }
 
-func (u64 *C.uint64aligned) Set(value uint64) {
+func SetUint64(u64 *C.uint64aligned, value uint64) {
 	u64.value[0] = C.uint32_t(value)
 	u64.value[1] = C.uint32_t(value >> 32)
 }
 
-func (i64 C.int64aligned) Get() int64 {
+func GetInt64(i64 C.int64aligned) int64 {
 	return int64(uint64(i64.value[0]) | uint64(i64.value[1])<<32)
 }
 
-func (i64 *C.int64aligned) Set(value int64) {
+func SetInt64(i64 *C.int64aligned, value int64) {
 	i64.value[0] = C.uint32_t(value)
 	i64.value[1] = C.uint32_t(uint64(value) >> 32)
 }
--- internal/aligned64.go
+++ internal/aligned64.go
@@ -11,18 +11,18 @@ typedef struct int64aligned { int64_t value[1]; } int64aligned;
 */
 import "C"
 
-func (u64 C.uint64aligned) Get() uint64 {
+func GetUint64(u64 C.uint64aligned) uint64 {
 	return uint64(u64.value[0])
 }
 
-func (u64 *C.uint64aligned) Set(value uint64) {
+func SetUint64(u64 *C.uint64aligned, value uint64) {
 	u64.value[0] = C.uint64_t(value)
 }
 
-func (i64 C.int64aligned) Get() int64 {
+func GetInt64(i64 C.int64aligned) int64 {
 	return int64(i64.value[0])
 }
 
-func (i64 *C.int64aligned) Set(value int64) {
+func SetInt64(i64 *C.int64aligned, value int64) {
 	i64.value[0] = C.int64_t(value)
 }
